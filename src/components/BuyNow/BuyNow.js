42169c58c9ffce32eb5c45f601df6c9eabe70bcc
Shashwat Kumar <shashwat.kumar1108@gmail.com>
2021-06-24 15:35:13 +0530

    Finally

--- src/components/BuyNow/BuyNow.js
+++ src/components/BuyNow/BuyNow.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import {Button} from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
 import Line from '../../assets/Group 4791.png';
 import './styles.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const BuyNow = () => {
+
+    const [title, setTitle]=useState("");
+    const [body, setBody]=useState("");
+    const [user, setUser]=useState("");
+
+    const titleHandler = (e) => {
+        setTitle(e.target.value);
+    }
+
+    const bodyHandler = (e) => {
+        setBody(e.target.value);
+    }
+
+    const userHandler = (e) => {
+        setUser(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const plan = {
+            t: title,
+            b: body,
+            u: user,
+        };
+        axios.post("https://jsonplaceholder.typicode.com/posts", plan)
+        .then((res) => window.alert('Success'));
+    }
+
     return (
         <div className="buy-container">
             <h1>Basic ZRP Plan</h1>
             <img src={Line} width={350} height={5} />
-            <Card>
-  <Card.Header>Featured</Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="json">
+                    JSON Placeholder Post
+                </div>
+                <div className="body">
+                    <div className="inputs">
+                        <div className="input-1"><input type="text" placeholder="Title" className="field" value={title} onChange={titleHandler}></input></div>
+                        <div className="input-2"><input type="text" placeholder="Body" className="field" value={body} onChange={bodyHandler}></input></div>
+                        <div className="input-3"><input type="text" placeholder="UserId" className="field" value={user} onChange={userHandler}></input></div>
+                    </div>
+                    <div>
+                        <input type="submit" className="submit"></input>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
